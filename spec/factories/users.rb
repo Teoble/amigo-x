eca45b788258216fbda1752253034b256178cda2
Patricia Furtado <patyfurtado_1989@hotmail.com>
2016-06-25 15:27:08 -0300

    validate email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :user do
-    name "MyString"
-    email "MyString"
+  factory :user_mail_valid, class: "User" do
+    name "user valid"
+    email "email@email.com"
+  end
+
+  factory :user_mail_not_valid, class: "User" do
+    name "user invalid"
+    email "email"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+describe User, type: :model do
+  context "validate mail" do
+    it "test if mail is valid" do
+      user = create(:user_mail_valid)
+      # user = User.new({name: 'jose', email: 'email@email.com'})
+      expect(user.valid?).to be true
+    end
+    it "test if mail is not valid" do
+      user = build(:user_mail_not_valid)
+      # user = User.new({name: 'jose', email: 'email@email.com'})
+      expect(user.valid?).to be false
+    end
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  end
