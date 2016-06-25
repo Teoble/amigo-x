7a4ae5dd70b36aaf25cbd8da062c4f730e05c4eb
Patricia Furtado <patyfurtado_1989@hotmail.com>
2016-06-25 17:01:22 -0300

    Resolver problema de subir a validacao do email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User, type: :model do
       # user = User.new({name: 'jose', email: 'email@email.com'})
       expect(user.valid?).to be true
     end
-    it "test if mail is not valid" do
+    it "test if mail is not valid" do 
       user = build(:user_mail_not_valid)
       # user = User.new({name: 'jose', email: 'email@email.com'})
       expect(user.valid?).to be false
