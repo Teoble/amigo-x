cc5e034d44a4998a9c0b64ac3bab34fcf30523f3
Gustavo Vinícius <gustavo@webgoal.com.br>
2016-06-26 11:52:27 -0300

    Mudanças no testo do app e redirect no sorteio e na inserção para a listagem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'Usuário foi adicionado com sucesso!' }
+        format.html { redirect_to users_url, notice: 'Usuário foi adicionado com sucesso!' }
         format.json { render :show, status:'createdEmail is invalid', location: @user }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'Usuário foi atualizado!' }
+        format.html { redirect_to users_url notice: 'Usuário foi atualizado!' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -65,7 +65,8 @@ class UsersController < ApplicationController
     # render :nothing => true
     User.raffle(User.all)
     User.all.each do |u| UserMailer.notify(u) end
-    redirect_to controller: 'users', action:'index'
+    respond_to do |format| format.html { redirect_to users_url, notice: 'Sorteio foi realizado!' } end
+    # redirect_to controller: 'users', action:'index'
   end
 
   private
